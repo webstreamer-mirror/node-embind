8e1f6bf7ea054f419400a2aafe38baa09290bf62
Mingyi Zhang <zhangmingyi@kedacom.com>
2019-02-15 18:16:43 +0800

    -a

--- conanfile.py
+++ conanfile.py
@@ -38,7 +38,7 @@ class NodeEmbind(ConanFile):
         self.run('echo ========================')
         self.run('pwd')
         self.run('pyenv root')
-        self.run('pyenv --versions')
+        self.run('pyenv versions')
         self.run('pyenv local 2 && echo XXXX && python --version')
         self.run('echo ========================')
 
