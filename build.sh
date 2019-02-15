a885a207431fac7b59c7fa39a03359f57862a03f
Mingyi Zhang <mingyi.z@outlook.com>
2019-02-15 10:47:36 +0000

    -a

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-
-sudo -E docker run --rm -v "$PWD:/home/conan/project" /bin/bash -c " cd project &&  npm install && node build.js && npm test"
+sudo -E docker run --rm -v $PWD:/home/conan/project webstreamer/node-gyp-gcc54:0.1  /bin/bash -c "source /home/conan/.nvm/nvm.sh && cd project && pyenv local 2.7.15 &&  npm install && node ./test/addons/build.js hello && npm test"
 
