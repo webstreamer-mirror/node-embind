29d6810edb01c0a2c476ba6906e76d51d54cb65f
Mingyi <mingyi.z@outlook.com>
2019-02-17 16:01:48 +0800

    .

--- test/addons/build.cmd
+++ test/addons/build.cmd
@@ -5,20 +5,64 @@ set _dirname=%~dp0
 set _PYTHON2=c:/python27
 set _DEBUG=
 set _ERROR=
-
 if "%NODE_ENV%" EQU Debug  set _DEBUG=--debug 
 
 if defined PYTHON set _PYTHON2=%PYTHON%
 
-if "%1" EQU "" (
-  call :BuildAll 
+set _configure=
+set _build=
+set _rebuild=
+set _project=
+
+set _action=%1
+set _project=%2
+
+
+if "%_action%"=="--configure" (
+  set _configure=Yes
+)
+
+if "%_action%"=="--rebuild" (
+  set _rebuil d=Yes
+)
+
+if "%_action%"=="--build" (
+  set _build=Yes
+)
+
+if "%_configure%%_rebuild%%_build%" EQU "" (
+  
+  if "%_project%" EQU "" (
+    set _project=%_action%
+    set _configure=Yes
+    set _rebuild=Yes
+  ) else (
+    echo Invalid command arguments, see help
+    call :Usage
+    set errorlevel=1
+    goto :_EXIT
+    
+  )
+)
+
+
+
+REM echo _configure=%_configure%
+REM echo _build=%_build%
+REM echo _rebuild=%_rebuild%
+REM echo _project=%_project%
+
+set errorlevel=0
+if "%_project%" EQU "" (
+  call :BuildAll
 ) else (
-  call :BuildOne %_dirname%%1
+  call :BuildOne %_dirname%%_project%
 )
 
 
 :_EXIT
 popd
+if defined _ERROR echo "%_ERROR%"
 goto :eof
 
 ::--------------------------------------------------------
@@ -26,30 +70,55 @@ goto :eof
 ::--------------------------------------------------------
 
 :BuildOne    - build addon
+
 set addon_dir=%1
 
-call npx node-gyp configure -C %addon_dir% --python %_PYTHON2%
-if %errorlevel% NEQ 0 (
-  set _ERROR="configure failed (%addon_dir%) !"
-  goto :_EXIT
+if "%_configure%" == "Yes" (
+
+  call npx node-gyp configure -C %addon_dir% --python %_PYTHON2%
+  
+  if %errorlevel% NEQ 0 (
+    echo "configure failed (%addon_dir%)"
+    goto :_EXIT
+  )
 )
 
-call npx node-gyp rebuild -C %addon_dir% --python %_PYTHON2%
-if %errorlevel% NEQ 0 (
-  set _ERROR="build failed (%addon_dir%) !"
-  goto :_EXIT
+if "%_build%" == "Yes" (
+
+  call npx node-gyp build -C %addon_dir% --python %_PYTHON2%
+  if %errorlevel% NEQ 0 (
+    echo  "build failed (%addon_dir%)"
+    goto :_EXIT
+  )
 )
+
+if "%_rebuild%" == "Yes" (
+
+  call npx node-gyp rebuild -C %addon_dir% --python %_PYTHON2%
+  if %errorlevel% NEQ 0 (
+    echo "rebuild failed (%addon_dir%)"
+    goto :_EXIT
+  )
+)
+
 goto:eof
 
 :BuildAll - build all addon folders
 
-for  /D  %%s in (%_dirname%\*)do (
-   set addon_dir=%%s
-   if exist %addon_dir%\binding.gyp (
-      call :BuildOne %addon_dir%
-   ) else (
-     echo skip %addon_dir% as no binding.gyp
-   )
+
+for /f "delims=" %%c in ('dir /D /b "%_dirname%"') do (  
+  if exist %_dirname%\%%c\binding.gyp (
+     call :BuildOne %_dirname%\%%c
+  ) 
 )
+
+goto :eof
+
+:Usage 
+  echo Usage: build [--build, --rebuild, --configure ] [project]
+  echo only do configure for hello project : build --configure hello
+  echo do build for all project in addons  : build --build
+  echo.
+
 goto :eof
 
