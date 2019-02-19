0a0902f1a7d4df3ca065520aefbdafda422a425e
Mingyi <mingyi.z@outlook.com>
2019-02-19 19:19:30 +0800

    * add name for docker conatainer

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 _dirname=$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
-sudo -E docker run --env NODE_ENV=$NODE_ENV --rm -v $_dirname:/home/ci/project webstreamer/node-gyp-gcc54 \
-  bash -c " source /opt/nvm/nvm.sh && cd project && $1"
+sudo -E docker run  --name node_embind_runner --env NODE_ENV=$NODE_ENV \
+     --rm -v $_dirname:/home/ci/project webstreamer/node-gyp-gcc54 \
+     bash -c " source /opt/nvm/nvm.sh && cd project && $1"
