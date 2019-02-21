cb6e64f7b4aa13920f2817f75dd14705d05faa42
Mingyi Zhang <zhangmingyi@kedacom.com>
2019-02-21 12:19:15 +0800

    .

--- test/debug.js
+++
@@ -1,8 +0,0 @@
-console.log('====  Begin Debug.js ===');
-
-const addons=require('./addons');
-const addon=addons.string;
-
-console.log(addon.EMBIND_STD_STRING_IS_UTF8');
-
-console.log('====  END Debug.js ===');
